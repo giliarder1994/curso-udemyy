815d08fdb305c8b076d9ccfc46a4f280c506b15c
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-09-16 20:42:48 -0300

    aula de else if

---
+++ SEÇÃO 4/AULA/12 - estrutura de controle else if/script.js
@@ -0,0 +1,16 @@
+let nome = Giliarde
+let idade = 31
+
+if (nome != undefined && nome == "Giliarde") {
+  console.log("Nome esta definido.")
+} else if (nome == "Giliarde" && nome.length > 5 && idade == 50) {
+  console.log("O nome é Giliarde")
+} else {
+  console.log("Não é Giliarde")
+}
+
+if (1 > 2) {
+  console.log("Teste")
+} else if (1 == 1) {
+  console.log("Testando")
+}
