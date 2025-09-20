9b972947bbbd2da52dcf1fee91356e694ed3f8c8
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-09-20 20:31:39 -0300

    seção 5

---
+++ SEÇÃO 5/AULA/1 - definindo uma função/script.js
@@ -0,0 +1,17 @@
+function imprimirNoConsole() {
+  console.log("Ola, mundo!")
+}
+imprimirNoConsole()
+
+function imprimirUmNumero(num) {
+  console.log("O número é :" + num)
+}
+imprimirUmNumero(2)
+imprimirUmNumero(4)
+imprimirUmNumero(9)
+imprimirUmNumero(20)
+
+const numeroAleatorio = function(){
+  console.log(Math.random())
+}
+numeroAleatorio()
