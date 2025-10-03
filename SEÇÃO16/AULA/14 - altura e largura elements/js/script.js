c79333e13aab8795e96a4a6995f1878a0a1ad036
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-10-02 22:20:03 -0300

    mais conteudo js

---
+++ SEÇÃO16/AULA/14 - altura e largura elements/js/script.js
@@ -0,0 +1,10 @@
+
+/*
+let elTest = document.getElementById('titulo')
+
+console.log(elTest.offsetWidth)//considera bordas
+console.log(elTest.offsetHeight)//considera bordas
+
+console.log(elTest.clientWidth) // desconsidera bordas
+console.log(elTest.clientHeight) // desconsidera bordas
+*/
